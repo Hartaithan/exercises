659c41e2c9c6c18c958fdab73293643d7eb747f1
Hartaithan <hartaithan@gmail.com>
2023-09-13 20:33:19 +0900

    add exercise-0

---
+++ src/exercise-0/solution.ts
@@ -0,0 +1,85 @@
+interface Badge {
+  [key: string | symbol]: string;
+  firstName: string;
+  lastName: string;
+  fullName: string;
+}
+
+const createBadge = (firstName: string, lastName: string): Badge => {
+  const handler: ProxyHandler<Badge> = {
+    get: (target, name) => {
+      if (name === "fullName") {
+        return `${target.firstName} ${target.lastName}`;
+      }
+      return target[name];
+    },
+    set: (obj, prop, value: string) => {
+      switch (prop) {
+        case "firstName":
+          obj.firstName = value;
+          return true;
+        case "lastName":
+          obj.lastName = value;
+          return true;
+        case "fullName":
+          if (value.includes(" ")) {
+            throw new TypeError("Error: invalid fullName format");
+          }
+          const [firstName, lastName] = value.split(" ");
+          obj.firstName = firstName;
+          obj.lastName = lastName;
+          obj.fullName = value;
+          return true;
+        default:
+          throw new TypeError("Error: unknown");
+      }
+    },
+  };
+  return new Proxy(
+    {
+      firstName,
+      lastName,
+      fullName: `${firstName} ${lastName}`,
+    },
+    handler
+  );
+};
+
+const badge = createBadge("Иван", "Иванов");
+
+console.log(badge.firstName);
+// Иван
+console.log(badge.lastName);
+// Иванов
+console.log(badge.fullName);
+// Иван Иванов
+
+badge.firstName = "Константин";
+
+console.log(badge.firstName);
+// Константин
+console.log(badge.lastName);
+// Иванов
+console.log(badge.fullName);
+// Константин Иванов
+
+badge.lastName = "Петров";
+
+console.log(badge.firstName);
+// Константин
+console.log(badge.lastName);
+// Петров
+console.log(badge.fullName);
+// Константин Петров
+
+badge.fullName = "Илон Маск";
+
+console.log(badge.firstName);
+// Илон
+console.log(badge.lastName);
+// Маск
+console.log(badge.fullName);
+// Илон Маск
+
+badge.fullName = "ИлонМаск";
+// Error: invalid fullName format
