a7fc71d3894b1fa5462a6d3d9ea06a0f09ecbe52
Hartaithan <hartaithan@gmail.com>
2023-09-17 11:16:36 +0900

    add lowercase transform

--- src/exercise-3/solution.js
+++ src/exercise-3/solution.js
@@ -4,9 +4,9 @@
  */
 const sortASC = (arr) => {
   return arr.sort((a, b) => {
-    const aLetter = a.slice(0, 1);
+    const aLetter = a.slice(0, 1).toLocaleLowerCase();
     const aNumber = Number(a.slice(1, a.length));
-    const bLetter = b.slice(0, 1);
+    const bLetter = b.slice(0, 1).toLocaleLowerCase();
     const bNumber = Number(b.slice(1, b.length));
     if (aLetter > bLetter) return 1;
     if (aLetter < bLetter) return -1;
