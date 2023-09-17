b7db08c6f7e7971257afceaf265fb01d170dac90
Hartaithan <hartaithan@gmail.com>
2023-09-17 10:58:10 +0900

    add exercise-3

---
+++ src/exercise-3/solution.js
@@ -0,0 +1,19 @@
+/**
+ * @param {String[]} arr
+ * @returns {String[]}
+ */
+const sortASC = (arr) => {
+  return arr.sort((a, b) => {
+    const aLetter = a.slice(0, 1);
+    const aNumber = Number(a.slice(1, a.length));
+    const bLetter = b.slice(0, 1);
+    const bNumber = Number(b.slice(1, b.length));
+    if (aLetter > bLetter) return 1;
+    if (aLetter < bLetter) return -1;
+    if (aNumber > bNumber) return 1;
+    if (aNumber < bNumber) return -1;
+  });
+};
+
+console.log(sortASC(["A100", "A3", "Z100", "Z2"]));
+// [ 'A3', 'A100', 'Z2', 'Z100' ]
